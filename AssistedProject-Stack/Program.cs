7aa5b64080cbbb775d8c8b2a6f72ad722447ed21
Kishore Kaladasi <kishorekaladasi2001@gmail.com>
2023-10-21 11:59:55 +0530

    Add project files.

---
+++ AssistedProject-Stack/Program.cs
@@ -0,0 +1,43 @@
+﻿using AssistedProject_Stack;
+using System;
+
+namespace AssistedProject_Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+
+        public static void runApp()
+        {
+            Stack stack = new Stack();
+
+            Console.WriteLine("length of stack=" + stack.getLengthOfStack());
+            if (stack.peek() == null)
+                Console.WriteLine("Stack contains nothing");
+            Console.WriteLine("**********************************");
+            for (int i = 0; i < 10; i++)
+            {
+                Node n = new Node();
+                n.Data = i.ToString();
+                stack.push(n);
+                Console.WriteLine("Length of stack=" + stack.getLengthOfStack());
+            }
+            Console.WriteLine("**********************************");
+            Node node = stack.pop();
+            Console.WriteLine("Popped " + node.Data);
+            Console.WriteLine("**********************************");
+            node = stack.pop();
+            Console.WriteLine("Popped " + node.Data);
+            Console.WriteLine("**********************************");
+
+            Console.WriteLine("Stack items");
+            Console.WriteLine(stack.listContents());
+            Console.WriteLine("**********************************");
+
+            Console.ReadLine();
+        }
+    }
+}
